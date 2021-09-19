f90794e7e156856b106a1b01d103f88eb8193563
Alisher Kim <cerberstrix@gmail.com>
2021-09-19 06:49:17 +0500

    fix barim-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,6 +2,9 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
 
-import { greeting } from '../src/cli.js';
+import { greeting, questionForName } from '../src/cli.js';
+import { greetingsByName } from '../src/gamesComponents.js';
 
 console.log(greeting);
+const name = questionForName();
+console.log(greetingsByName(name));
