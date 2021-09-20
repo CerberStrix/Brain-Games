80101530d24300ebf2ea87a3a4f7dc40625e7de9
Alisher Kim <cerberstrix@gmail.com>
2021-09-20 21:00:56 +0500

    fix progression again

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -2,6 +2,6 @@
 /* eslint-disable import/extensions */
 import { termsOfPrimeGame } from '../src/gamesComponents.js';
 import generalLogic from '../src/index.js';
-import primeLogic from '../games/gcd.js';
+import primeLogic from '../games/prime.js';
 
 console.log(generalLogic(termsOfPrimeGame, primeLogic));
