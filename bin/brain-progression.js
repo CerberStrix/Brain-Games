870d6bc7352c1ff3349edf2cf4824263432914f6
Alisher Kim <cerberstrix@gmail.com>
2021-09-20 20:58:20 +0500

    fix progression again

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -2,6 +2,6 @@
 /* eslint-disable import/extensions */
 import { termsOfProgressionGame } from '../src/gamesComponents.js';
 import generalLogic from '../src/index.js';
-import progressionLogic from '../games/gcd.js';
+import progressionLogic from '../games/progression.js';
 
 console.log(generalLogic(termsOfProgressionGame, progressionLogic));
